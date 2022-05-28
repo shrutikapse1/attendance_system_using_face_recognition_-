ac60639a4b6fae036b19df21e7001fb4258f209a
shruti kapse <81238358+shrutikapse1@users.noreply.github.com>
2022-05-28 18:20:49 +0530

    modified train.py

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ class Train:
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System ")
+        self.root.wm_iconbitmap("favicon.ico")
 
         title_lbl=Label(self.root,text="Train Dataset ",font=("times new roman",35,"bold"),bg="white",fg="DarkGreen")
         title_lbl.place(x=0,y=0,width=1540,height=45)
